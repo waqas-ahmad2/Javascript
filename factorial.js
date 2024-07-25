ab604c2453423f675225423f1be38edd10130794
Waqas Ahmad <97921489+waqas-ahmad2@users.noreply.github.com>
2024-07-25 00:10:27 -0400

    Updated factorial.js
    added new method to create an array using Array.from

--- factorial.js
+++ factorial.js
@@ -8,6 +8,7 @@ function factorial_recursion(num){
     return factorial(num-1)*num
 
 }
+console.log(factorial_recursion(5))
 
 // using for loop
 
@@ -33,10 +34,16 @@ function arr(num){
     }
     return res
 };
-
 console.log(arr(5).reduce((a,b)=>a*b))
 
-console.log(factorial_recursion(5))
+
+//we can also make arrays with this code
+let num = 6
+let arr_new = Array.from(Array(num+1).keys()) //num+1 because array starts from 0 (0...num+1) 
+
+//slice array from index 1 to num 
+console.log(arr_new.slice(1,).reduce((a,b)=>a*b))
+
 
 
 
